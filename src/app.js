ebfd4ef876a2f8fb0039bb674eff93474c96cfaa
eduardocarlostoledo <eduardocarlostoledo@gmail.com>
2024-11-05 19:57:30 -0300

    implementacion de helmet, passport, oauth2, prevension de xms hjks scripting

--- src/app.js
+++ src/app.js
@@ -1,16 +1,19 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
+const passport = require("passport");
+const session = require("express-session");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const cors = require("cors");
-const fileUpload = require('express-fileupload');
-const morgan = require('morgan');
-const helmet = require('helmet'); // Importa helmet
+const fileUpload = require("express-fileupload");
+const morgan = require("morgan");
+const helmet = require("helmet"); // Importa helmet
 const mercadopago = require("mercadopago");
 
-const routes = require('./routes/index.js');
-require('./db.js');
+const routes = require("./routes/index.js");
+require("./db.js");
 
 const server = express();
-server.name = 'API';
+server.name = "API";
 
 // Configuración de MercadoPago
 const ACCESS_TOKEN_MERCADOPAGO = process.env.ACCESS_TOKEN_MERCADOPAGO;
@@ -19,77 +22,78 @@ if (ACCESS_TOKEN_MERCADOPAGO) {
     accessToken: ACCESS_TOKEN_MERCADOPAGO,
   });
 } else {
-  console.error("Error: ACCESS_TOKEN_MERCADOPAGO no está definido en el archivo .env");
+  console.error(
+    "Error: ACCESS_TOKEN_MERCADOPAGO no está definido en el archivo .env"
+  );
 }
 
 // Middleware de seguridad - Helmet
 server.use(
-  
   helmet({
     contentSecurityPolicy: {
       directives: {
         defaultSrc: ["'self'"],
         scriptSrc: [
           "'self'",
-          "https://ajax.googleapis.com",            // jQuery desde Google CDN
-          "https://sdk.mercadopago.com",             // SDK de MercadoPago
-          "https://accounts.google.com",             // Google Sign-In
-          "https://cdn.jsdelivr.net",                // Bootstrap desde jsDelivr
-          "https://unpkg.com",                       // Ionicons desde unpkg
+          "https://ajax.googleapis.com", // jQuery desde Google CDN
+          "https://sdk.mercadopago.com", // SDK de MercadoPago
+          "https://accounts.google.com", // Google Sign-In
+          "https://cdn.jsdelivr.net", // Bootstrap desde jsDelivr
+          "https://unpkg.com", // Ionicons desde unpkg
           "https://opencollective.com/babel",
           "https://github.com",
           "https://eslint.org",
           "https://vitejs.dev",
           "https://cdn-icons-png.flaticon.com",
-          "https://wa.me",                           // WhatsApp
-          "https://login.live.com",                  // Microsoft Login
+          "https://wa.me", // WhatsApp
+          "https://login.live.com", // Microsoft Login
         ],
         styleSrc: [
           "'self'",
-          "https://cdn.jsdelivr.net",                // Bootstrap CSS desde jsDelivr
-          "https://fonts.googleapis.com",            // Google Fonts
+          "https://cdn.jsdelivr.net", // Bootstrap CSS desde jsDelivr
+          "https://fonts.googleapis.com", // Google Fonts
         ],
         imgSrc: [
-          "'self'", 
-          "data:",                                  // Permite cargar imágenes en línea
+          "'self'",
+          "data:", // Permite cargar imágenes en línea
           "https://elgatonegropremium.netlify.app", // Imágenes locales
-          "https://res.cloudinary.com",             // Cloudinary
+          "https://res.cloudinary.com", // Cloudinary
           "https://cdn-icons-png.flaticon.com",
           "https://www.instagram.com",
           "https://www.facebook.com",
           "https://web.whatsapp.com",
         ],
         connectSrc: [
-          "'self'", 
+          "'self'",
           "https://api.mercadopago.com",
           "https://elgatonegropremium-back-production.up.railway.app", // Backend API
           "https://opencollective.com",
           "https://www.robotstxt.org",
           "https://www.instagram.com",
           "https://web.whatsapp.com",
-          "https://wa.me"
+          "https://wa.me",
         ],
         fontSrc: [
           "'self'",
-          "https://fonts.gstatic.com",              // Google Fonts
-          "https://cdn.jsdelivr.net",               // Fuentes de Bootstrap desde jsDelivr
+          "https://fonts.gstatic.com", // Google Fonts
+          "https://cdn.jsdelivr.net", // Fuentes de Bootstrap desde jsDelivr
         ],
         frameSrc: [
-          "https://www.mercadopago.com",            // iframes de MercadoPago
-          "https://login.live.com",                 // Microsoft Login
+          "https://www.mercadopago.com", // iframes de MercadoPago
+          "https://login.live.com", // Microsoft Login
         ],
-        objectSrc: ["'none'"],                      // Bloquea elementos <object>
+        objectSrc: ["'none'"], // Bloquea elementos <object>
         mediaSrc: ["'self'", "https://res.cloudinary.com"], // Permite solo contenido multimedia desde Cloudinary
       },
     },
-  })  
-
+  })
 );
 
 // Configuración de CORS
 const corsOptions = {
   origin: function (origin, callback) {
     const whitelist = [
+      "https://accounts.google.com",
       "http://localhost:5173",
       "https://localhost",
       "https://elgatonegropremium.netlify.app",
@@ -108,14 +112,18 @@ const corsOptions = {
       "https://login.live.com",
       "https://elgatonegropremium-back-production.up.railway.app",
       "https://res.cloudinary.com",
-      'https://api.mercadopago.com',  // MercadoPago
-      'https://www.mercadolibre.com', // MercadoLibre
-      'https://www.google.com'        // Google
+      "https://api.mercadopago.com", // MercadoPago
+      "https://www.mercadolibre.com", // MercadoLibre
+      "https://www.google.com", // Google
     ];
     if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true, console.log("peticion de origen aceptada desde ",origin));
+      callback(
+        null,
+        true,
+        console.log("peticion de origen aceptada desde ", origin)
+      );
     } else {
-      callback(new Error('No permitida por CORS', origin));
+      callback(new Error("No permitida por CORS", origin));
     }
   },
   credentials: true,
@@ -123,16 +131,18 @@ const corsOptions = {
 server.use(cors(corsOptions));
 
 // Middleware de carga de archivos
-server.use(fileUpload({
-  useTempFiles: true,
-  tempFileDir: './uploads'
-}));
+server.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "./uploads",
+  })
+);
 
 // Middleware de registro de solicitudes HTTP con Morgan
-server.use(morgan('dev'));
+server.use(morgan("dev"));
 
 // Definir un token personalizado para el ID de la solicitud
-morgan.token('id', function (req) {
+morgan.token("id", function (req) {
   return req.id;
 });
 
@@ -144,7 +154,9 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Configurar Morgan con un formato personalizado
-server.use(morgan(':id :method :url :status :response-time ms - :res[content-length]'));
+server.use(
+  morgan(":id :method :url :status :response-time ms - :res[content-length]")
+);
 
 // Middleware para agregar un ID único a cada solicitud
 server.use((req, res, next) => {
@@ -161,7 +173,7 @@ const validateUrlMiddleware = (req, res, next) => {
   if (req.body.url) {
     const urlRegex = /^(ftp|http|https):\/\/[^ "]+$/;
     if (!urlRegex.test(req.body.url)) {
-      return res.status(400).json({ error: 'Invalid URL' });
+      return res.status(400).json({ error: "Invalid URL" });
     }
   }
   next();
@@ -171,10 +183,43 @@ const validateUrlMiddleware = (req, res, next) => {
 server.use(validateUrlMiddleware);
 
 // Rutas
-server.use('/', routes);
+server.use("/", routes);
+
+server.use(
+  session({
+    secret: process.env.JWT_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+server.use(passport.initialize());
+server.use(passport.session());
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.CLIENT_ID_LOGIN,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      // Aquí puedes guardar el perfil del usuario en la base de datos o sesión
+      return done(null, profile);
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
 
 // Middleware para manejo de errores
-server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+server.use((err, req, res, next) => {
+  // eslint-disable-line no-unused-vars
   const status = err.status || 500;
   const message = err.message || err;
   console.error(err);
@@ -182,113 +227,3 @@ server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
 });
 
 module.exports = server;
-
-// require('dotenv').config();
-// const express = require('express');
-// const cors = require("cors");
-// const fileUpload = require('express-fileupload');
-// const morgan = require('morgan');
-// const helmet = require("helmet");
-// const mercadopago = require("mercadopago");
-
-// const routes = require('./routes/index.js');
-// require('./db.js');
-
-// const server = express();
-
-// server.name = 'API';
-
-// // Configuración de MercadoPago
-// const ACCESS_TOKEN_MERCADOPAGO = process.env.ACCESS_TOKEN_MERCADOPAGO;
-// if (ACCESS_TOKEN_MERCADOPAGO) {
-//   mercadopagoClient = new mercadopago.MercadoPagoConfig({
-//     accessToken: ACCESS_TOKEN_MERCADOPAGO,
-//   });
-// } else {
-//   console.error("Error: ACCESS_TOKEN_MERCADOPAGO no está definido en el archivo .env");
-// }
-
-// // Middleware
-// server.use(morgan('dev')); // middleware de registro de solicitudes HTTP para Node.js
-
-
-// // Configuración de CORS
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     const whitelist = [
-//       "http://localhost:5173",
-//       "https://elgatonegropremium.netlify.app",
-//       'https://elgatonegropremium.netlify.app/',  // Netlify
-//       'https://elgatonegropremium-back-production.up.railway.app',          // Railway
-//       'https://api.mercadopago.com',            // MercadoPago
-//       'https://www.mercadolibre.com',           // MercadoLibre
-//       'https://www.google.com'                  // Google
-//     ];
-//     if (whitelist.indexOf(origin) !== -1 || !origin) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   },
-//   credentials: true,
-// };
-// server.use(cors(corsOptions));
-
-// server.use(fileUpload({
-//   useTempFiles: true,
-//   tempFileDir: './uploads'
-// }));
-
-// console.log("La instancia es: ", process.env.NODE_ENV);
-
-// // Definir un token personalizado para el ID de la solicitud
-// morgan.token('id', function (req) {
-//   return req.id;
-// });
-
-// // Condicional para definir la instancia de desarrollo
-// if (process.env.NODE_ENV === "production") {
-//   server.use(morgan("combined"));
-// } else {
-//   server.use(morgan("dev"));
-// }
-
-// // Configurar Morgan con un formato personalizado
-// server.use(morgan(':id :method :url :status :response-time ms - :res[content-length]'));
-
-// // Middleware para agregar un ID único a cada solicitud
-// server.use((req, res, next) => {
-//   req.id = Math.random().toString(36).substring(7);
-//   next();
-// });
-
-// // Middlewares de Express para parsear JSON y datos URL-encoded
-// server.use(express.json());
-// server.use(express.urlencoded({ extended: false }));
-
-// // Middleware global para validar URL
-// const validateUrlMiddleware = (req, res, next) => {
-//   if (req.body.url) {
-//     const urlRegex = /^(ftp|http|https):\/\/[^ "]+$/;
-//     if (!urlRegex.test(req.body.url)) {
-//       return res.status(400).json({ error: 'Invalid URL' });
-//     }
-//   }
-//   next();
-// };
-
-// // Aplicar validación de URL a nivel global
-// server.use(validateUrlMiddleware);
-
-// // Rutas
-// server.use('/', routes);
-
-// // Middleware para manejo de errores
-// server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
-//   const status = err.status || 500;
-//   const message = err.message || err;
-//   console.error(err);
-//   res.status(status).send(message);
-// });
-
-// module.exports = server;
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,86 +1,121 @@
-const { Router } = require('express');
-const { putUser, getUsers, getUserId, loginUser, postUsers, deleteUser, postUserGoogle, loginGoogle, findUser, recuperarPassword } = require("../controllers/usersController.js")
-const {verificaToken} = require("../helpers/verificaToken.js")
-const {verifyAdmin} = require("../helpers/verifyAdmin.js")
-const userRouter = Router()
+const { Router } = require("express");
+const passport=require("passport");
+const {
+  putUser,
+  getUsers,
+  getUserId,
+  loginUser,
+  postUsers,
+  deleteUser,
+  postUserGoogle,
+  loginGoogle,
+  findUser,
+  recuperarPassword,
+} = require("../controllers/usersController.js");
+const { verificaToken } = require("../helpers/verificaToken.js");
+const { verifyAdmin } = require("../helpers/verifyAdmin.js");
 
-userRouter.get("/verificaUsuario", verificaToken)
-userRouter.get("/verificaAdmin", verifyAdmin)
-//////////////////////////////// CREAR USUARIO /////////////////////////////////////// 
-userRouter.post('/register', postUsers) // users/register
+const userRouter = Router();
 
-//////////////////////////////// CREAR USUARIO /////////////////////////////////////// 
-userRouter.post('/google', postUserGoogle) // users/google
+userRouter.get("/verificaUsuario", verificaToken);
+userRouter.get("/verificaAdmin", verifyAdmin);
+//////////////////////////////// CREAR USUARIO ///////////////////////////////////////
+userRouter.post("/register", postUsers); // users/register
 
-//////////////////////////////// INICIAR SESSION  /////////////////////////////////////// 
-userRouter.post('/login', loginUser)
-userRouter.post("/loginGoogle", loginGoogle)
+//////////////////////////////// CREAR USUARIO ///////////////////////////////////////
+userRouter.post("/google", postUserGoogle); // users/google
 
-//////////////////////////////// MODIFICAR USUARIO  /////////////////////////////////////// 
+//////////////////////////////// INICIAR SESSION  ///////////////////////////////////////
+userRouter.post("/login", loginUser);
+userRouter.post("/loginGoogle", loginGoogle);
+
+userRouter.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
+
+// Ruta de callback para Google
+userRouter.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/" }),
+  (req, res) => {
+    // Redirige al usuario a la página deseada tras un inicio de sesión exitoso
+    res.redirect("/dashboard");
+  }
+);
+
+// Ruta de logout
+userRouter.get("/logout", (req, res) => {
+  req.logout(() => {
+    res.redirect("/");
+  });
+});
+
+//////////////////////////////// MODIFICAR USUARIO  ///////////////////////////////////////
 userRouter.put("/:id", async (req, res) => {
   const { id } = req.params;
   let image = false;
   if (req.files) image = req.files.image;
   try {
-    const user = await putUser(req.body, image, id)
-    res.status(200).json("Usuario actualizado")
+    const user = await putUser(req.body, image, id);
+    res.status(200).json("Usuario actualizado");
   } catch (error) {
-    res.status(400).json(error.message)
+    res.status(400).json(error.message);
   }
-})
+});
 
 ///////////////////////////// PERMITIR CAMBIO DE CONTRASEÑA Y ENVIO DE MAIL /////////////////////////////
-userRouter.post("/changePass", recuperarPassword)
+userRouter.post("/changePass", recuperarPassword);
 
 //ruta para verificar si user existe y si es valida la exp reg...
 userRouter.get("/", async (req, res) => {
   const regex_FullText = /^([a-zA-Z ]+)/i;
 
   const { name } = req.query;
-  console.log("ruta de get/",name)
-  let users
+  console.log("ruta de get/", name);
+  let users;
 
   try {
     if (name) {
       if (name.trim() === "") {
         users = await getUsers();
-        res.status(200).json({ data: users, message: "Listado de usuarios" })
+        res.status(200).json({ data: users, message: "Listado de usuarios" });
       } else {
         if (regex_FullText.test(name)) {
-
-          users = await findUser(name.trim());  // aca
+          users = await findUser(name.trim()); // aca
           if (users.length == 0) {
             res.status(500).json({
               status: false,
               msg: `No se encontro ningun User con el atributo ${name}`,
-              errorCode: 12
-            })
-
+              errorCode: 12,
+            });
           } else {
-            res.status(200).json({ data: users, message: "Listado de usuarios" })
+            res
+              .status(200)
+              .json({ data: users, message: "Listado de usuarios" });
           }
         } else {
           res.status(500).json({
             status: false,
             msg: `Formato de busqueda invalido`,
-            errorCode: 14
+            errorCode: 14,
           });
         }
       }
     } else {
       users = await getUsers();
-      res.status(200).json({ data: users, message: "Listado de usuarios" })
+      res.status(200).json({ data: users, message: "Listado de usuarios" });
     }
-
-
   } catch (error) {
     res.status(400).json({
       status: false,
       msg: `Entro al catch, ${error.message}`,
-      errorCode: 400
+      errorCode: 400,
     });
   }
-})
+});
 
 //////////////////////////////// TRAER USUARIO POR PARAMETRO ////////////////////////////////
 userRouter.get("/:id", async (req, res) => {
@@ -98,6 +133,6 @@ userRouter.get("/:id", async (req, res) => {
 });
 
 //////////////////////////////// BORRAR USUARIO ////////////////////////////////
-userRouter.delete('/delete/:id', deleteUser)
+userRouter.delete("/delete/:id", deleteUser);
 
-module.exports = { userRouter }
+module.exports = { userRouter };
